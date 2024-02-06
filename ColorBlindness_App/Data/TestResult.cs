998085ae1f7861809012a6353fec8e8e2839114a
norhan khaled <norhank252@gmail.com>
2024-02-06 10:53:59 +0200

    create folder data create class user create class test create class testresult

---
+++ ColorBlindness_App/Data/TestResult.cs
@@ -0,0 +1,14 @@
+﻿namespace ColorBlindness_App.Data
+{
+    public class TestResult
+    {
+        public int TestResult_Id { get; set; }
+        public int CorrectAnswer {  get; set; }
+        public int WrongAnswer { get; set; }
+        public double Accuracy { get; set; }
+        public bool IsColorBlind {  get; set; }
+        public virtual User User_Id { get; set; }
+        public virtual Test Test_Id { get; set; }
+        public DateTime DateRecorded { get; set; }
+    }
+}
